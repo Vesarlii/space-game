20ac964f35e4ab7820f87d832cf631fd8c2c25ed
Pysia <bkubinska111@gmail.com>
2024-02-17 20:20:17 +0100

    add first version

---
+++ game.js
@@ -0,0 +1,10 @@
+var config = {
+    width: 1024,
+    height: 512,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true
+  }
+
+  
+  var game = new Phaser.Game(config);
