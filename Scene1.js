341af5e350b61608a15d0bfc5fb2edb954d65cbb
Pysia <bkubinska111@gmail.com>
2024-02-17 21:14:02 +0100

    add new background and objects

--- Scene1.js
+++ Scene1.js
@@ -4,16 +4,16 @@ class Scene1 extends Phaser.Scene {
     }
   
     preload(){
+      this.load.image("background00", "assets/images/background00.png");
       this.load.image("background", "assets/images/background.png");
       this.load.image("ship", "assets/images/ship.png");
       this.load.image("ship2", "assets/images/ship2.png");
       this.load.image("ship3", "assets/images/ship3.png");
-      this.load.image("colors", "assets/colors.png");
     }
   
     create() {
       this.add.text(20, 20, "Loading game...");
       this.scene.start("playGame");
-      this.ship.setScale(2);
+
     }
   }
--- Scene2.js
+++ Scene2.js
@@ -4,8 +4,10 @@ class Scene2 extends Phaser.Scene {
     }
   
     create() {
-      // 4.1 make the background a tile sprite
-      //this.background = this.add.image(0, 0, "background");
+
+
+      this.background00 = this.add.tileSprite(0, 0, config.width, config.height, "background00");
+      this.background00.setOrigin(0, 0);
       this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
       this.background.setOrigin(0, 0);
   
@@ -20,35 +22,35 @@ class Scene2 extends Phaser.Scene {
   
     }
   
-    // 0 add the update function
+ 
     update() {
   
-      // 1.1 call a function to move the ships vertically
+  
       this.moveShip(this.ship1, 5);
       this.moveShip(this.ship2, 2);
       this.moveShip(this.ship3, 3);
   
-      // 4.2 scroll the background
       this.background.tilePositionY -= 0.5;
+      this.background00.tilePositionY -= 0.3;
   
     }
   
-    // 1.2 create the function to move the ships
+
     moveShip(ship, speed) {
-      // increase the position of the ship on the vertical axis
+  
       ship.y += speed;
-      // if the ship hits the bottom of the screen call the reset function
+
       if (ship.y > config.height) {
-        // 2.1 call a reset position function
+   
         this.resetShipPos(ship);
       }
     }
   
-    // 2.2 create the reset position function
+  
     resetShipPos(ship){
-      // put the ship on the top
+ 
       ship.y = 0;
-      // put the ship on a random position on the x axis
+ 
       var randomX = Phaser.Math.Between(0, config.width);
       ship.x = randomX;
     }
